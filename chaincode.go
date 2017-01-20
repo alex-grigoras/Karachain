96e29d3d5c2d840173f2b2a4fa05dd325f5b1993
alex-grigoras <alex.grigoras@gmail.com>
2017-01-20 15:44:38 +0200

    Create chaincode.go

---
+++ chaincode.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+
+"github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
+)
+
+const(
+VideoTable = "Videos"
+RatingTable = "Ratings"
+UserTable = "Users"
+SingerTable = "Singer"
+ProducerTable = "Producer"
+ContractTable = "Contract"
+)
+type Karachain struct{}
+
+func main() {
+
+	err := shim.Start(new(Karachain))
+	if err != nil {
+		fmt.Println("Error starting Karachain: %s", err)
+	}
+}
+
+func (t *Karachain) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
+	return nil, nil
+}
+
+func (t *Karachain) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func (t *Karachain) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Print("Hello Karachain!")
+	return nil, nil
+}
