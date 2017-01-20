9a527a23714da8f957aaef9a0bfdc8dc015ce27b
alex-grigoras <alex.grigoras@gmail.com>
2017-01-20 16:13:24 +0200

    Update chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -33,6 +33,6 @@ func (t *Karachain) Invoke(stub shim.ChaincodeStubInterface, function string, ar
 }
 
 func (t *Karachain) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	shim.NewLogger("gigi"). Critical("Hello " + args[0] +"!")
+	shim.NewLogger("gigi").Critical("Hello " + args[0] +"!")
 	return nil, nil
 }
